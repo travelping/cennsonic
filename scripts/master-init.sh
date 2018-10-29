e79d92f25f768a1b3faae250e26852658d40ac6a
Anton Alferov <anton.alferov@travelping.com>
2018-10-29 10:26:18 +0100

    Remove obsolete scripts

--- scripts/master-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export KUBECONFIG=/etc/kubernetes/admin.conf
-
-sudo kubeadm init --config kubeadm-config.yaml
-
-sudo -E kubectl apply -f calico-rbac-kdd.yaml
-sudo -E kubectl apply -f calico.yaml
-sudo -E kubectl apply -f keepalived.yaml
